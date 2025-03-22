15f2a8b4804f0ab1b7cadacc4dca325ce59ba9cc
Aditya Prakash <46171746+imadi04@users.noreply.github.com>
2025-03-22 17:06:16 +0530

    Create gcp-vpc-and-subnets.tf

---
+++ misc/gcp-vpc-and-subnets.tf
@@ -0,0 +1,18 @@
+provider "google" {
+  project = "my-gcp-project"
+  region  = "us-central1"
+}
+
+# Create a VPC Network
+resource "google_compute_network" "vpc_network" {
+  name                    = "my-vpc"
+  auto_create_subnetworks = false  # Set to false to manually create subnets
+}
+
+# Create a Subnet
+resource "google_compute_subnetwork" "subnet" {
+  name          = "my-subnet"
+  network       = google_compute_network.vpc_network.id
+  ip_cidr_range = "10.1.0.0/16"
+  region        = "us-central1"
+}
